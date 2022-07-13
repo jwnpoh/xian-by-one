9919abd7ef05f4d65426a0c1a9e34d9726694758
jwnpoh <jwn.poh@gmail.com>
2022-07-14 02:11:08 +0800

    remember scroll position in programme booklet

--- src/Prog.svelte
+++ src/Prog.svelte
@@ -1,11 +1,40 @@
 <script>
+  import { onMount, beforeUpdate, afterUpdate, onDestroy } from "svelte";
+
   export let imgPath;
+  let scroll;
+
+  onMount(() => {
+    console.log("programme is mounted");
+    scroll = localStorage.getItem("scroll");
+    console.log("last y position is ", scroll);
+    scrollTo({ top: scroll });
+  });
+
+  onDestroy(() => {
+    console.log("programme is destroyed");
+    console.log("y position is ", scroll);
+    localStorage.setItem("scroll", scroll);
+  });
+
+  beforeUpdate(() => {
+    console.log("before update");
+  });
+
+  afterUpdate(() => {
+    console.log("after update");
+  });
 </script>
 
-<img src={imgPath} alt="Programme Booklet" />
+<div class="img">
+  <img src={imgPath} alt="Programme Booklet" />
+</div>
+
+<svelte:window bind:scrollY={scroll} />
 
 <style>
-  img {
+  img,
+  .img {
     width: 100%;
   }
 </style>
